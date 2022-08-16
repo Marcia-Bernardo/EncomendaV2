4c47e1bf938d243abf2e2a5e8e17990120b6de12
Márcia Regina Bernardo <79754243+Marcia-Bernardo@users.noreply.github.com>
2022-08-16 22:31:00 +0100

    color legenda

--- components/Legend.js
+++ components/Legend.js
@@ -4,16 +4,32 @@ const Legend = () => {
   return (
     <div>
       Legenda:
-      <button type="button" className="btn btn-warning mx-3">
+      <button
+        type="button"
+        className="btn btn-warning mx-3"
+        style={{ backgroundColor: "yellow" }}
+      >
         iniciar o preparo
       </button>
-      <button type="button" className="btn btn-danger mx-3">
+      <button
+        type="button"
+        className="btn btn-danger mx-3"
+        style={{ backgroundColor: "red" }}
+      >
         preparo atrasado
       </button>
-      {/* <button type="button" className="btn btn-orange mx-3">
+      <button
+        type="button"
+        className="btn btn-orange mx-3"
+        style={{ backgroundColor: "orange" }}
+      >
         à preparar
-      </button> */}
-      <button type="button" className="btn btn-success mx-3">
+      </button>
+      <button
+        type="button"
+        className="btn btn-success mx-3"
+        style={{ backgroundColor: "green" }}
+      >
         pronto
       </button>
     </div>
